cf2ee3e93236252e429fdf3a58bbee73113a6a18
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-09 16:55:36 -0400

    feat: add keybinding for nvimtree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,4 +1,4 @@
 
 -- nvim tree line bindings
 vim.keymap.set("n", "<leader>pt", vim.cmd.NvimTreeFocus) -- focus nvim tree
-vim.keymap.set("n", "<leader>ptc", vim.cmd.NvimTreeClose) -- close nvim tree
+vim.keymap.set("n", "<leader>ptc", vim.cmd.NvimTreeToggle) -- toggle nvim tree

066925d642efe921829b04a629d90d712ddebb12
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-09 16:19:29 -0400

    feat: change theme color from darker to palenight

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,5 @@
-
 -- Setting the colorscheme
 vim.cmd 'colorscheme material'
 -- Color scheme
-vim.g.material_style = "darker" 
+vim.g.material_style = "palenight" 
 -- darker, lighter, oceanic, palenight, deep ocean

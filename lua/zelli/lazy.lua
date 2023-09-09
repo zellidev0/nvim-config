e794c9c203abddf677f189803f49dceefcefbe1a
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-09 16:21:27 -0400

    feat: add nvim-tree to nvim config

--- lua/zelli/lazy.lua
+++ lua/zelli/lazy.lua
@@ -1,46 +1,68 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- 	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	"folke/neodev.nvim",
-	{ 
-		"nvim-telescope/telescope.nvim", 
-		tag = "0.1.2", 
-		dependencies = { 
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{ "marko-cerovac/material.nvim" },
-	{ "ThePrimeagen/vim-be-good" },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "mbbill/undotree" },
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+  { 
+    "nvim-telescope/telescope.nvim", 
+    tag = "0.1.2", 
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  { "marko-cerovac/material.nvim" },
+  { "ThePrimeagen/vim-be-good" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "mbbill/undotree" },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	}
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      }
+    end,
+  }
 })
 
 

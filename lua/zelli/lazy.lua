ae0e81a6b9bd8604f4d6ac2e2767e06a15d307cc
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-10 15:02:30 -0400

    fmt: format lazy.lua

--- lua/zelli/lazy.lua
+++ lua/zelli/lazy.lua
@@ -56,15 +56,9 @@ require("lazy").setup({
     config = function()
       require("nvim-tree").setup {
         sort_by = "case_sensitive",
-        view = {
-          width = 30,
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = true,
-        },
+        view = { width = 30 },
+        renderer = { group_empty = true },
+        filters = { dotfiles = true },
       }
     end,
   }

8987e115fb1d17c1d3fc4fc5bd641c176e74a3ab
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-10 19:16:35 -0400

    feat: add lua-lsp to lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,7 +19,7 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 -- (Optional) Configure lua language server for neovim
--- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.ensure_installed({
 	'gopls'

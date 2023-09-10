0dd8b07c4e4e4cf76b7d76c68fd13ce39bee85d9
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-10 19:15:51 -0400

    feat: remove unnecessary languages from lsp

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "javascript", "typescript", "kotlin", "dart", "rust", "lua", "vim", "vimdoc", "query", "go" },
+    ensure_installed = { "c", "dart", "lua", "vim", "vimdoc", "query", "go" },
     sync_install = false,
     auto_install = true,
 

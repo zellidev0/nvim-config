4eb035fbf11b92cae0997f8f3a6bfc38e9183842
Julian Zellner <zelli@Julians-Air.localdomain>
2023-09-15 21:05:35 -0400

    feat: move nvim-tree plugin setup to separate file

--- lua/zelli/lazy.lua
+++ lua/zelli/lazy.lua
@@ -81,13 +81,5 @@ require("lazy").setup({
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-      require("nvim-tree").setup {
-        sort_by = "case_sensitive",
-        view = { width = 30 },
-        renderer = { group_empty = true },
-        filters = { dotfiles = true },
-      }
-    end,
   }
 })
